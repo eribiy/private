9de570575da69c457cfd66d1add82d68b74db2f8
eribiy <katerina.ribiy@gmail.com>
2017-05-26 17:49:10 +0300

    test work continue

--- test work/scripts/script.js
+++ test work/scripts/script.js
@@ -1,3 +1,37 @@
 /**
- * Created by kate on 24.05.17.
+ * Created by kate on 25.05.17.
  */
+$(document).ready(function(){
+    $('.accordion').click(function(){
+		$(this).toggleClass('arrowClose').toggleClass('arrowOpen');
+     
+    	var panel = $(this).next();
+        if (panel.css('display') === "block") {
+        	panel.css('display', 'none');
+        } else {
+        	panel.css('display', 'block');
+        } 
+    });   
+});
+
+
+
+function openTab(evt, tabName) {
+    var i, tabcontent, tablinks;
+
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+
+    tablinks = document.getElementsByClassName("info-tab-link");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+
+    document.getElementById(tabName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
+
+
+
