d13cb4582e55df7dc014c02bef2983b7b3cc350b
eribiy <katerina.ribiy@gmail.com>
2017-09-07 18:12:57 +0300

    adding slider

--- test_project/scripts/script.js
+++ test_project/scripts/script.js
@@ -12,13 +12,40 @@ $(document).ready(function() {
         panel.toggle();
     });
 */
+
+    var slider = $('.bxslider').bxSlider({
+        controls: false,
+        startSlide: 1
+    });
+  
     $('.info-tab-link').click(function () {
         $('.info-tab-link').removeClass('active');
         $(this).addClass("active");
 
         $('.tabcontent').removeClass('active');
-        $('#'+ $(this).data('tab')).addClass('active');
+        $('#'+ $(this).data('tab')).addClass('active');/*.find('.bxslider').bxSlider({
+        controls: false,
+        startSlide: 1
+
+    });
+*/
+        event.preventDefault();
+        slider.reloadSlider();
+
     });
 
+
+
+    /*(function($) {
+        $(function() {
+
+          $('ul.info-tabs-wrapper').on('click', 'li:not(.active)', function() {
+            $(this)
+              .addClass('active').siblings().removeClass('active')
+              .closest('div.tabs').find('div.tabcontent').removeClass('active').eq($(this).index()).addClass('active');
+          });
+
+        });
+        })(jQuery);*/
     
 });
